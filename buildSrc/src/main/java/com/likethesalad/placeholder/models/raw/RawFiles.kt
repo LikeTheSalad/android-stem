ecea0e05dc2eb7f996bd04282631ab0dfb0ba7e0
Cesar <amqcesar@gmail.com>
2019-12-24 11:16:42 +0000

    Clean up unused classes

--- buildSrc/src/main/java/com/likethesalad/placeholder/models/raw/RawFiles.kt
+++
@@ -1,7 +0,0 @@
-package com.likethesalad.placeholder.models.raw
-
-import java.io.File
-
-abstract class RawFiles(val suffix: String, val mainValuesRawFiles: List<File>) {
-    abstract fun getRawFilesMetaList(): List<List<File>>
-}
