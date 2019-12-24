0a19ba72b1e3ed16e60a455b1ea14c69c5680e94
Cesar <amqcesar@gmail.com>
2019-12-24 12:26:28 +0000

    Creating incremental dirs if don't exist

--- buildSrc/src/main/java/com/likethesalad/placeholder/data/storage/IncrementalDirsProvider.kt
+++ buildSrc/src/main/java/com/likethesalad/placeholder/data/storage/IncrementalDirsProvider.kt
@@ -4,13 +4,7 @@ import com.likethesalad.placeholder.data.helpers.AndroidVariantHelper
 import java.io.File
 
 class IncrementalDirsProvider(androidVariantHelper: AndroidVariantHelper) {
-    private val incrementalDir: File by lazy {
-        val dir = File(androidVariantHelper.incrementalDir)
-        if (!dir.exists()) {
-            dir.createNewFile()
-        }
-        dir
-    }
+    private val incrementalDir = File(androidVariantHelper.incrementalDir)
 
     fun getRawStringsDir(): File {
         return getIncrementalDirAndCreateIfNotExists("strings")
@@ -23,7 +17,7 @@ class IncrementalDirsProvider(androidVariantHelper: AndroidVariantHelper) {
     private fun getIncrementalDirAndCreateIfNotExists(dirName: String): File {
         val dir = File(incrementalDir, dirName)
         if (!dir.exists()) {
-            dir.createNewFile()
+            dir.mkdirs()
         }
 
         return dir
