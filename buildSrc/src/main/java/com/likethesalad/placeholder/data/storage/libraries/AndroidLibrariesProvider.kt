d13453006ae75c2bc150e06f30131a20dfe31710
Cesar <amqcesar@gmail.com>
2020-04-10 15:43:55 +0100

    Retrieving android libraries

--- buildSrc/src/main/java/com/likethesalad/placeholder/data/storage/libraries/AndroidLibrariesProvider.kt
+++ buildSrc/src/main/java/com/likethesalad/placeholder/data/storage/libraries/AndroidLibrariesProvider.kt
@@ -1,7 +1,12 @@
 package com.likethesalad.placeholder.data.storage.libraries
 
 import com.likethesalad.placeholder.data.helpers.AndroidConfigHelper
+import com.likethesalad.placeholder.models.AndroidLibrary
 
 class AndroidLibrariesProvider(private val androidConfigHelper: AndroidConfigHelper) {
 
+    fun getAndroidLibraries(): Set<AndroidLibrary> {
+        val artifacts = androidConfigHelper.getResArtifactCollection().artifacts
+        return artifacts.map { AndroidLibrary(it.id.displayName, it.file) }.toSet()
+    }
 }
--- buildSrc/src/test/java/com/likethesalad/placeholder/data/storage/libraries/AndroidLibrariesProviderTest.kt
+++ buildSrc/src/test/java/com/likethesalad/placeholder/data/storage/libraries/AndroidLibrariesProviderTest.kt
@@ -1,10 +1,18 @@
 package com.likethesalad.placeholder.data.storage.libraries
 
+import com.google.common.truth.Truth
 import com.likethesalad.placeholder.data.helpers.AndroidConfigHelper
+import com.likethesalad.placeholder.models.AndroidLibrary
+import io.mockk.every
 import io.mockk.mockk
+import org.gradle.api.artifacts.ArtifactCollection
+import org.gradle.api.artifacts.component.ComponentArtifactIdentifier
+import org.gradle.api.artifacts.result.ResolvedArtifactResult
 import org.junit.Before
 import org.junit.Test
+import java.io.File
 
+@Suppress("UnstableApiUsage")
 class AndroidLibrariesProviderTest {
     private lateinit var androidConfigHelper: AndroidConfigHelper
     private lateinit var androidLibrariesProvider: AndroidLibrariesProvider
@@ -17,16 +25,38 @@ class AndroidLibrariesProviderTest {
 
     @Test
     fun getAndroidLibraries() {
+        val name1 = "someLib"
+        val file1 = mockk<File>()
+        val name2 = "someLib2"
+        val file2 = mockk<File>()
+        val artifacts = createMockArtifactsFor(
+            Pair(name1, file1),
+            Pair(name2, file2)
+        )
+        setResolvableDependenciesWith(artifacts)
 
+        val libraries = androidLibrariesProvider.getAndroidLibraries()
+
+        Truth.assertThat(libraries).containsExactly(
+            AndroidLibrary(name1, file1),
+            AndroidLibrary(name2, file2)
+        )
     }
 
-//    @Suppress("UnstableApiUsage")
-//    private fun createResolvableDependenciesWith(artifacts: Set<ResolvedArtifactResult>)
-//            : ResolvableDependencies {
-//        val resolvableDependencies = mockk<ResolvableDependencies>()
-//        val viewConfig = mockk<ArtifactView.ViewConfiguration>()
-//        val actionCaptor = slot<Double>()
-//
-//
-//    }
+    private fun setResolvableDependenciesWith(artifacts: Set<ResolvedArtifactResult>) {
+        val artifactCollection = mockk<ArtifactCollection>()
+        every { artifactCollection.artifacts }.returns(artifacts)
+        every { androidConfigHelper.getResArtifactCollection() }.returns(artifactCollection)
+    }
+
+    private fun createMockArtifactsFor(vararg names: Pair<String, File>): Set<ResolvedArtifactResult> {
+        return names.map { item ->
+            mockk<ResolvedArtifactResult>().also {
+                val id = mockk<ComponentArtifactIdentifier>()
+                every { id.displayName }.returns(item.first)
+                every { it.id }.returns(id)
+                every { it.file }.returns(item.second)
+            }
+        }.toSet()
+    }
 }
