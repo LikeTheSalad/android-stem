1d218417000a8d926abda8e96842cae237f85e38
Cesar <amqcesar@gmail.com>
2021-01-03 10:57:48 +0000

    Clean up

--- string-reference/src/test/java/com/likethesalad/placeholder/data/ValuesFolderXmlFilesTest.kt
+++ string-reference/src/test/java/com/likethesalad/placeholder/data/ValuesFolderXmlFilesTest.kt
@@ -1,7 +1,6 @@
 package com.likethesalad.placeholder.data
 
 import com.google.common.truth.Truth
-import com.likethesalad.placeholder.modules.common.models.StringResourceModel
 import com.likethesalad.placeholder.modules.rawStrings.data.helpers.files.ValuesFolderXmlFiles
 import org.junit.Test
 import java.io.File
@@ -23,23 +22,6 @@ class ValuesFolderXmlFilesTest {
 
         Truth.assertThat(valuesFiles.valuesFolderName).isEqualTo("values")
         Truth.assertThat(valuesFiles.xmlFiles).containsExactlyElementsIn(valuesFilesSet)
-//        Truth.assertThat(valuesFiles.stringResources).containsExactly( todo for other test
-//            StringResourceModel(
-//                "welcome_1",
-//                "The welcome message for TesT1"
-//            ),
-//            StringResourceModel(
-//                "welcome_3",
-//                "The welcome message for TesT3"
-//            ),
-//            StringResourceModel(
-//                mapOf(
-//                    "name" to "message_non_translatable_1",
-//                    "translatable" to "false"
-//                ),
-//                "Non translatable TesT2"
-//            )
-//        )
     }
 
     private fun getResourceFile(fileName: String): File {
