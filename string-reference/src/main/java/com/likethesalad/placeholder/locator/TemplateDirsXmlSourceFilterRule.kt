121092ff120cda3aed456f0f75f66b5b928a044c
Cesar <likethesalad@gmail.com>
2022-01-11 15:16:27 +0000

    Created TemplateDirsXmlSourceFilterRule to exclude template dirs from android resource extractor

---
+++ string-reference/src/main/java/com/likethesalad/placeholder/locator/TemplateDirsXmlSourceFilterRule.kt
@@ -0,0 +1,25 @@
+package com.likethesalad.placeholder.locator
+
+import com.likethesalad.placeholder.modules.common.helpers.android.AndroidVariantContext
+import com.likethesalad.tools.resource.collector.android.filter.BaseAndroidXmlSourceFilterRule
+import com.likethesalad.tools.resource.collector.android.source.AndroidXmlResourceSource
+
+class TemplateDirsXmlSourceFilterRule(androidVariantContext: AndroidVariantContext) : BaseAndroidXmlSourceFilterRule() {
+
+    private val templatesDirHandler by lazy { androidVariantContext.templatesDirHandler }
+    private val templateBaseDirRegex: Regex by lazy {
+        val dirPaths = templatesDirHandler.templatesDirs.map { it.dir.path }
+        Regex(dirPaths.fold("") { accumulated: String, currentPath: String ->
+            val startsWithRegex = getStartsWithRegex(currentPath)
+            if (accumulated.isEmpty()) startsWithRegex else "$accumulated|$startsWithRegex"
+        })
+    }
+
+    private fun getStartsWithRegex(path: String): String {
+        return "^${path}.+"
+    }
+
+    override fun doExclude(source: AndroidXmlResourceSource): Boolean {
+        return templateBaseDirRegex.matches(source.getFileSource().path)
+    }
+}
