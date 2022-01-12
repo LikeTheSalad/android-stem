2df3fe65fd24a651d1ae7279c42ba059a4b6542b
Cesar <likethesalad@gmail.com>
2022-01-12 15:54:49 +0000

    Created module for templates provider

---
+++ templates-provider/src/main/java/com/likethesalad/android/templates/provider/TemplatesProviderPlugin.kt
@@ -0,0 +1,11 @@
+package com.likethesalad.android.templates.provider
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class TemplatesProviderPlugin : Plugin<Project> {
+
+    override fun apply(project: Project) {
+
+    }
+}
