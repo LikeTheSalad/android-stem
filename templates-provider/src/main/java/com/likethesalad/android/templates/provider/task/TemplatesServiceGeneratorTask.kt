a9b820013916c813e48c39c4f8918688129a75a0
Cesar <likethesalad@gmail.com>
2022-01-27 18:23:23 +0000

    Created task for templates provider

---
+++ templates-provider/src/main/java/com/likethesalad/android/templates/provider/task/TemplatesServiceGeneratorTask.kt
@@ -0,0 +1,12 @@
+package com.likethesalad.android.templates.provider.task
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+open class TemplatesServiceGeneratorTask : DefaultTask() {
+
+    @TaskAction
+    fun execute() {
+
+    }
+}
---
+++ templates-provider/src/main/java/com/likethesalad/android/templates/provider/task/action/TemplatesServiceGeneratorAction.kt
@@ -0,0 +1,10 @@
+package com.likethesalad.android.templates.provider.task.action
+
+import com.likethesalad.tools.resource.locator.android.extension.configuration.data.ResourcesProvider
+
+class TemplatesServiceGeneratorAction(private val templatesProvider: ResourcesProvider) {
+
+    fun execute() {
+
+    }
+}
