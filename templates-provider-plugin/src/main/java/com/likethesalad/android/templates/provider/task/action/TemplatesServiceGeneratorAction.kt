7eabb8387470a24142a34d6b8d3bbcd64ed9d257
Cesar <likethesalad@gmail.com>
2022-01-28 07:26:55 +0000

    Created RandomStringGenerator

--- templates-provider-plugin/src/main/java/com/likethesalad/android/templates/provider/task/action/TemplatesServiceGeneratorAction.kt
+++ templates-provider-plugin/src/main/java/com/likethesalad/android/templates/provider/task/action/TemplatesServiceGeneratorAction.kt
@@ -1,8 +1,21 @@
 package com.likethesalad.android.templates.provider.task.action
 
+import com.likethesalad.android.templates.provider.task.action.helpers.RandomStringGenerator
 import com.likethesalad.tools.resource.locator.android.extension.configuration.data.ResourcesProvider
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
 
-class TemplatesServiceGeneratorAction(private val templatesProvider: ResourcesProvider) {
+class TemplatesServiceGeneratorAction @AssistedInject constructor(
+    @Assisted private val projectName: String,
+    @Assisted private val templatesProvider: ResourcesProvider,
+    private val randomStringGenerator: RandomStringGenerator
+) {
+
+    @AssistedFactory
+    interface Factory {
+        fun create(projectName: String, templatesProvider: ResourcesProvider): TemplatesServiceGeneratorAction
+    }
 
     fun execute() {
 
