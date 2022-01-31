4361b79fd035c9fffffeb29b7bb7237b0be6cec3
Cesar <likethesalad@gmail.com>
2022-01-31 06:48:08 +0000

    Created ClassNameGenerator

---
+++ templates-provider-plugin/src/main/java/com/likethesalad/android/templates/provider/task/action/helpers/ClassNameGenerator.kt
@@ -0,0 +1,36 @@
+package com.likethesalad.android.templates.provider.task.action.helpers
+
+import java.util.Base64
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ClassNameGenerator @Inject constructor(
+    private val randomStringGenerator: RandomStringGenerator,
+    private val base64Encoder: Base64.Encoder
+) {
+
+    companion object {
+        private const val PACKAGE_NAME = "com.likethesalad.android.templates.provider.implementation"
+        private const val NAME_PREFIX = "A_"
+        private val BASE64_TO_JAVA_IDENTIFIER = mapOf(
+            "=" to "_",
+            "+" to "€",
+            "/" to "$"
+        )
+    }
+
+    fun generate(identifier: String): String {
+        val raw = "${randomStringGenerator.generate()}_$identifier"
+        val encoded = base64Encoder.encodeToString(raw.toByteArray())
+        val curated = clearBase64NonAlphanumerics(encoded)
+
+        return "${PACKAGE_NAME}.$NAME_PREFIX$curated"
+    }
+
+    private fun clearBase64NonAlphanumerics(encoded: String): String {
+        return BASE64_TO_JAVA_IDENTIFIER.entries.fold(encoded) { text, entry ->
+            text.replace(entry.key, entry.value)
+        }
+    }
+}
