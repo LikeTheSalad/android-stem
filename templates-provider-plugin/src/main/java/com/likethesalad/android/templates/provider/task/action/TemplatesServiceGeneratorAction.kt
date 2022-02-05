206e2f87216d6bf923e19ecfb17af4575f70eee2
Cesar <likethesalad@gmail.com>
2022-02-05 13:50:39 +0000

    Creating TemplatesProvider implementation in TemplatesServiceGeneratorAction

--- templates-provider-plugin/src/main/java/com/likethesalad/android/templates/provider/task/action/TemplatesServiceGeneratorAction.kt
+++ templates-provider-plugin/src/main/java/com/likethesalad/android/templates/provider/task/action/TemplatesServiceGeneratorAction.kt
@@ -1,16 +1,19 @@
 package com.likethesalad.android.templates.provider.task.action
 
+import com.likethesalad.android.templates.provider.api.TemplatesProvider
 import com.likethesalad.android.templates.provider.task.action.helpers.ClassNameGenerator
 import com.likethesalad.tools.plugin.metadata.api.PluginMetadata
 import com.likethesalad.tools.resource.locator.android.extension.configuration.data.ResourcesProvider
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import net.bytebuddy.ByteBuddy
+import net.bytebuddy.implementation.FixedValue
+import net.bytebuddy.matcher.ElementMatchers.named
 import java.io.File
 
 class TemplatesServiceGeneratorAction @AssistedInject constructor(
     @Assisted private val projectName: String,
-    @Assisted private val templatesProvider: ResourcesProvider,
     @Assisted private val outputDir: File,
     private val classNameGenerator: ClassNameGenerator,
     private val pluginMetadata: PluginMetadata
@@ -26,6 +29,39 @@ class TemplatesServiceGeneratorAction @AssistedInject constructor(
     }
 
     fun execute() {
+        val className = classNameGenerator.generate(projectName)
+        val classBytes = createProviderImplementation(className)
 
+        val outputFile = createOutputFile(className)
+        outputFile.writeBytes(classBytes)
+    }
+
+    private fun createOutputFile(className: String): File {
+        val classFilePath = className.replace(".", "/")
+        val file = File(outputDir, "$classFilePath.class")
+        val classDir = file.parentFile
+        if (!classDir.exists()) {
+            classDir.mkdirs()
+        }
+
+        return file
+    }
+
+    private fun createProviderImplementation(className: String): ByteArray {
+        val pluginVersion = pluginMetadata.version
+        val templates = getTemplates()
+
+        return ByteBuddy()
+            .subclass(TemplatesProvider::class.java)
+            .name(className)
+            .method(named("getId")).intercept(FixedValue.value(projectName))
+            .method(named("getTemplates")).intercept(FixedValue.value(templates))
+            .method(named("getPluginVersion")).intercept(FixedValue.value(pluginVersion))
+            .make()
+            .bytes
+    }
+
+    private fun getTemplates(): String {
+        return ""
     }
 }
