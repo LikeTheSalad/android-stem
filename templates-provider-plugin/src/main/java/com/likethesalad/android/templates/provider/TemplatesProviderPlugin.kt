2451e77b335de7bd19659ddad08c19929aec3688
Cesar <likethesalad@gmail.com>
2022-02-05 16:01:20 +0000

    Connecting service generator to android build

--- templates-provider-plugin/src/main/java/com/likethesalad/android/templates/provider/TemplatesProviderPlugin.kt
+++ templates-provider-plugin/src/main/java/com/likethesalad/android/templates/provider/TemplatesProviderPlugin.kt
@@ -1,11 +1,45 @@
 package com.likethesalad.android.templates.provider
 
-import org.gradle.api.Plugin
+import com.likethesalad.android.templates.provider.di.TemplatesProviderComponent
+import com.likethesalad.android.templates.provider.di.TemplatesProviderInjector
+import com.likethesalad.android.templates.provider.task.TemplatesServiceGeneratorTask
+import com.likethesalad.tools.android.plugin.base.AndroidToolsPluginConsumer
+import com.likethesalad.tools.android.plugin.data.AndroidVariantData
 import org.gradle.api.Project
+import org.gradle.api.tasks.TaskProvider
 
-class TemplatesProviderPlugin : Plugin<Project> {
+@Suppress("UnstableApiUsage")
+class TemplatesProviderPlugin : AndroidToolsPluginConsumer() {
+
+    companion object {
+        private const val SERVICE_GENERATOR_TASK_TEMPLATE = "templateProvider%sMetadata"
+    }
+
+    private lateinit var component: TemplatesProviderComponent
 
     override fun apply(project: Project) {
+        super.apply(project)
+        component = TemplatesProviderInjector.getComponent()
 
+        androidTools.onVariant {
+            attachTasks(project, it)
+        }
+    }
+
+    private fun attachTasks(project: Project, variant: AndroidVariantData) {
+        val serviceGenerator = createServiceGeneratorTask(project, variant.getVariantName())
+
+        variant.registerGeneratedJavaBinaries(serviceGenerator, serviceGenerator.flatMap { it.outputDir })
+    }
+
+    private fun createServiceGeneratorTask(
+        project: Project,
+        variantName: String
+    ): TaskProvider<TemplatesServiceGeneratorTask> {
+        return project.tasks.register(
+            SERVICE_GENERATOR_TASK_TEMPLATE.format(variantName.capitalize()),
+            TemplatesServiceGeneratorTask::class.java,
+            component.templatesServiceGeneratorArgs()
+        )
     }
 }
--- templates-provider-plugin/src/main/java/com/likethesalad/android/templates/provider/di/TemplatesProviderComponent.kt
+++ templates-provider-plugin/src/main/java/com/likethesalad/android/templates/provider/di/TemplatesProviderComponent.kt
@@ -1,10 +1,11 @@
 package com.likethesalad.android.templates.provider.di
 
+import com.likethesalad.android.templates.provider.task.TemplatesServiceGeneratorTask
 import dagger.Component
 import javax.inject.Singleton
 
 @Component(modules = [TemplatesProviderModule::class])
 @Singleton
 interface TemplatesProviderComponent {
-
+    fun templatesServiceGeneratorArgs(): TemplatesServiceGeneratorTask.Args
 }
