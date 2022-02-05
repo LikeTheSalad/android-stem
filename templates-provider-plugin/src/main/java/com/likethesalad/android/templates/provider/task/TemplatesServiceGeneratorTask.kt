07da0c1e86feb56114d9045451fbb03a83e203f4
Cesar <likethesalad@gmail.com>
2022-02-05 13:56:31 +0000

    Using action inside TemplatesServiceGeneratorTask

--- templates-provider-plugin/src/main/java/com/likethesalad/android/templates/provider/task/TemplatesServiceGeneratorTask.kt
+++ templates-provider-plugin/src/main/java/com/likethesalad/android/templates/provider/task/TemplatesServiceGeneratorTask.kt
@@ -1,12 +1,25 @@
 package com.likethesalad.android.templates.provider.task
 
+import com.likethesalad.android.templates.provider.task.action.TemplatesServiceGeneratorAction
 import org.gradle.api.DefaultTask
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
+import javax.inject.Inject
+import javax.inject.Singleton
 
-open class TemplatesServiceGeneratorTask : DefaultTask() {
+@Suppress("UnstableApiUsage")
+open class TemplatesServiceGeneratorTask @Inject constructor(private val args: Args) : DefaultTask() {
+
+    @OutputDirectory
+    val outputDir: DirectoryProperty = project.objects.directoryProperty()
 
     @TaskAction
     fun execute() {
-
+        val action = args.actionFactory.create(project.name, outputDir.get().asFile)
+        action.execute()
     }
+
+    @Singleton
+    data class Args @Inject constructor(val actionFactory: TemplatesServiceGeneratorAction.Factory)
 }
--- templates-provider-plugin/src/main/java/com/likethesalad/android/templates/provider/task/action/TemplatesServiceGeneratorAction.kt
+++ templates-provider-plugin/src/main/java/com/likethesalad/android/templates/provider/task/action/TemplatesServiceGeneratorAction.kt
@@ -3,7 +3,6 @@ package com.likethesalad.android.templates.provider.task.action
 import com.likethesalad.android.templates.provider.api.TemplatesProvider
 import com.likethesalad.android.templates.provider.task.action.helpers.ClassNameGenerator
 import com.likethesalad.tools.plugin.metadata.api.PluginMetadata
-import com.likethesalad.tools.resource.locator.android.extension.configuration.data.ResourcesProvider
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
@@ -23,7 +22,6 @@ class TemplatesServiceGeneratorAction @AssistedInject constructor(
     interface Factory {
         fun create(
             projectName: String,
-            templatesProvider: ResourcesProvider,
             outputDir: File
         ): TemplatesServiceGeneratorAction
     }
