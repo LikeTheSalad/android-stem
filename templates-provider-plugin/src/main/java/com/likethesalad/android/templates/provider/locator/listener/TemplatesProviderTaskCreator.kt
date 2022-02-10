2b8ec3dbf83918c9491c31a93f13f51907068bdb
Cesar <likethesalad@gmail.com>
2022-02-10 06:16:40 +0000

    Using templates identifier task in templates provider

--- templates-provider-plugin/src/main/java/com/likethesalad/android/templates/provider/locator/listener/TemplatesProviderTaskCreator.kt
+++ templates-provider-plugin/src/main/java/com/likethesalad/android/templates/provider/locator/listener/TemplatesProviderTaskCreator.kt
@@ -1,12 +1,13 @@
 package com.likethesalad.android.templates.provider.locator.listener
 
+import com.likethesalad.android.templates.common.tasks.identifier.TemplatesIdentifierTask
+import com.likethesalad.android.templates.common.tasks.identifier.action.TemplatesIdentifierAction
 import com.likethesalad.android.templates.provider.tasks.metainf.ServiceMetaInfGeneratorTask
 import com.likethesalad.android.templates.provider.tasks.service.TemplatesServiceGeneratorTask
 import com.likethesalad.android.templates.provider.tasks.service.action.TemplatesServiceGeneratorAction
 import com.likethesalad.tools.android.plugin.data.AndroidVariantData
 import com.likethesalad.tools.resource.collector.android.data.variant.VariantTree
 import com.likethesalad.tools.resource.locator.android.extension.configuration.data.ResourceLocatorInfo
-import com.likethesalad.tools.resource.locator.android.extension.configuration.data.ResourcesProvider
 import com.likethesalad.tools.resource.locator.android.extension.listener.ResourceLocatorCreationListener
 import org.gradle.api.Project
 import org.gradle.api.tasks.TaskProvider
@@ -14,7 +15,8 @@ import org.gradle.api.tasks.TaskProvider
 @Suppress("UnstableApiUsage")
 class TemplatesProviderTaskCreator(
     private val project: Project,
-    private val taskServiceGeneratorActionFactory: TemplatesServiceGeneratorAction.Factory
+    private val taskServiceGeneratorActionFactory: TemplatesServiceGeneratorAction.Factory,
+    private val templatesIdentifierActionFactory: TemplatesIdentifierAction.Factory
 ) : ResourceLocatorCreationListener {
 
     companion object {
@@ -27,12 +29,12 @@ class TemplatesProviderTaskCreator(
     }
 
     private fun attachTasks(variant: AndroidVariantData, resourcesInfo: ResourceLocatorInfo) {
-        val serviceGenerator =
-            createServiceGeneratorTask(project, variant.getVariantName(), resourcesInfo.resourcesProvider)
+        val serviceGenerator = createServiceGeneratorTask(project, variant.getVariantName())
+        val templatesIdentifier = createTemplatesIdentifierTask(variant.getVariantName(), resourcesInfo)
         val metaInfGenerator = createMetaInfGeneratorTask(project, variant.getVariantName())
 
         serviceGenerator.configure {
-            it.rawResourcesDir.set(resourcesInfo.taskInfo.outputDirectoryProvider.getOutputDirProperty())
+            it.templateIdsFile.set(templatesIdentifier.flatMap { templatesIdentifier -> templatesIdentifier.outputFile })
         }
         metaInfGenerator.configure { task ->
             task.generatedClasspath.set(serviceGenerator.flatMap { it.outputDir })
@@ -46,13 +48,12 @@ class TemplatesProviderTaskCreator(
 
     private fun createServiceGeneratorTask(
         project: Project,
-        variantName: String,
-        rawResources: ResourcesProvider
+        variantName: String
     ): TaskProvider<TemplatesServiceGeneratorTask> {
         return project.tasks.register(
             SERVICE_GENERATOR_TASK_TEMPLATE.format(variantName.capitalize()),
             TemplatesServiceGeneratorTask::class.java,
-            TemplatesServiceGeneratorTask.Args(taskServiceGeneratorActionFactory, rawResources)
+            TemplatesServiceGeneratorTask.Args(taskServiceGeneratorActionFactory)
         )
     }
 
@@ -64,5 +65,25 @@ class TemplatesProviderTaskCreator(
             META_INF_GENERATOR_TASK_TEMPLATE.format(variantName.capitalize()),
             ServiceMetaInfGeneratorTask::class.java
         )
+    }
+
+    private fun createTemplatesIdentifierTask(
+        variantName: String,
+        localResourcesInfo: ResourceLocatorInfo
+    ): TaskProvider<TemplatesIdentifierTask> {
+        val provider = project.tasks.register(
+            TemplatesIdentifierTask.generateTaskName(variantName),
+            TemplatesIdentifierTask::class.java,
+            TemplatesIdentifierTask.Args(
+                localResourcesInfo.resourcesProvider,
+                templatesIdentifierActionFactory
+            )
+        )
+
+        provider.configure {
+            it.localResourcesDir.set(localResourcesInfo.taskInfo.outputDirectoryProvider.getOutputDirProperty())
+        }
+
+        return provider
     }
 }
