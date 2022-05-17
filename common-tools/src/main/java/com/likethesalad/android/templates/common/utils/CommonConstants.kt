fa20071e5986461eedd3eef9feef811076984f95
Cesar <likethesalad@gmail.com>
2022-05-17 17:42:37 +0100

    Using Matcher.quoteSeparator for FILE_SEPARATOR_MATCHER

--- common-tools/src/main/java/com/likethesalad/android/templates/common/utils/CommonConstants.kt
+++ common-tools/src/main/java/com/likethesalad/android/templates/common/utils/CommonConstants.kt
@@ -1,10 +1,11 @@
 package com.likethesalad.android.templates.common.utils
 
 import java.io.File
+import java.util.regex.Matcher
 
 object CommonConstants {
     val PLACEHOLDER_REGEX = Regex("\\$\\{([a-zA-Z0-9_]+)}")
     const val PROVIDER_PACKAGE_NAME = "com.likethesalad.android.templates.provider.implementation"
     const val RESOLVE_PLACEHOLDERS_TASKS_GROUP_NAME = "stem"
-    val FILE_SEPARATOR_MATCHER: String = File.separator
+    val FILE_SEPARATOR_MATCHER: String = Matcher.quoteReplacement(File.separator)
 }
