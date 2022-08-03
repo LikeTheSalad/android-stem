1641b6c86d0469b5f1a584bcbb050bf27e1c89bd
Cesar Munoz <likethesalad@gmail.com>
2022-08-03 08:38:38 +0200

    Added support for namespaced keys to XmlUtils

--- stem-plugin/src/main/java/com/likethesalad/stem/modules/common/helpers/resources/utils/XmlUtils.kt
+++ stem-plugin/src/main/java/com/likethesalad/stem/modules/common/helpers/resources/utils/XmlUtils.kt
@@ -1,20 +1,36 @@
 package com.likethesalad.stem.modules.common.helpers.resources.utils
 
 import com.likethesalad.stem.modules.common.Constants.Companion.XML_STRING_TAG
+import com.likethesalad.tools.resource.api.android.attributes.AndroidAttributeKey
 import com.likethesalad.tools.resource.api.android.modules.string.StringAndroidResource
 import org.w3c.dom.Document
 import org.w3c.dom.Element
 
-class XmlUtils {
+object XmlUtils {
 
-    companion object {
-        fun stringResourceModelToElement(document: Document, stringResourceModel: StringAndroidResource): Element {
-            val strElement = document.createElement(XML_STRING_TAG)
-            strElement.textContent = stringResourceModel.stringValue()
-            for (it in stringResourceModel.attributes().asMap()) {
-                strElement.setAttribute(it.key.value(), it.value)
+    fun stringResourceModelToElement(
+        document: Document, stringResourceModel: StringAndroidResource,
+        namespaceNameProvider: NamespaceNameProvider
+    ): Element {
+        val strElement = document.createElement(XML_STRING_TAG)
+        strElement.textContent = stringResourceModel.stringValue()
+        for (it in stringResourceModel.attributes().asMap()) {
+            when (it.key) {
+                is AndroidAttributeKey.Plain -> strElement.setAttribute(it.key.value(), it.value)
+                is AndroidAttributeKey.Namespaced -> strElement.setAttribute(
+                    getKeyName(it.key as AndroidAttributeKey.Namespaced, namespaceNameProvider),
+                    it.value
+                )
             }
-            return strElement
         }
+        return strElement
+    }
+
+    private fun getKeyName(namespacedKey: AndroidAttributeKey.Namespaced, provider: NamespaceNameProvider): String {
+        return "${provider.getNameFor(namespacedKey.namespaceValue)}:${namespacedKey.value()}"
+    }
+
+    interface NamespaceNameProvider {
+        fun getNameFor(namespaceValue: String): String
     }
 }
--- stem-plugin/src/test/java/com/likethesalad/stem/utils/XmlUtilsTest.kt
+++ stem-plugin/src/test/java/com/likethesalad/stem/utils/XmlUtilsTest.kt
@@ -2,11 +2,14 @@ package com.likethesalad.stem.utils
 
 import com.google.common.truth.Truth
 import com.likethesalad.stem.modules.common.helpers.resources.utils.XmlUtils
+import com.likethesalad.tools.resource.api.android.attributes.namespaced
 import com.likethesalad.tools.resource.api.android.attributes.plain
 import com.likethesalad.tools.resource.api.android.environment.Language
 import com.likethesalad.tools.resource.api.android.environment.Variant
 import com.likethesalad.tools.resource.api.android.impl.AndroidResourceScope
 import com.likethesalad.tools.resource.api.android.modules.string.StringAndroidResource
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.Test
 import javax.xml.parsers.DocumentBuilderFactory
 
@@ -25,14 +28,50 @@ class XmlUtilsTest {
                     plain("extra") to "some extra attr"
                 ), "some content", scope
             )
+        val noOpNsProvider = object : XmlUtils.NamespaceNameProvider {
+            override fun getNameFor(namespaceValue: String): String {
+                throw UnsupportedOperationException()
+            }
+        }
 
         // When:
-        val result = XmlUtils.stringResourceModelToElement(document, stringResourceModel)
+        val result = XmlUtils.stringResourceModelToElement(
+            document,
+            stringResourceModel,
+            noOpNsProvider
+        )
 
         // Then:
         Truth.assertThat(result.textContent).isEqualTo("some content")
         Truth.assertThat(result.attributes.length).isEqualTo(2)
         Truth.assertThat(result.attributes.getNamedItem("name").textContent).isEqualTo("some_name")
         Truth.assertThat(result.attributes.getNamedItem("extra").textContent).isEqualTo("some extra attr")
+    }
+
+    @Test
+    fun checkNamespacedStringResourceModelToElement() {
+        // Given:
+        val document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument()
+        val namespaceValue = "http://some.namespace"
+        val stringResourceModel =
+            StringAndroidResource(
+                mapOf(
+                    plain("name") to "some_name",
+                    plain("extra") to "some extra attr",
+                    namespaced("someNsKey", namespaceValue) to "Some namespaced value"
+                ), "some content", scope
+            )
+        val nsNameProvider = mockk<XmlUtils.NamespaceNameProvider>()
+        every { nsNameProvider.getNameFor(namespaceValue) }.returns("ns1")
+
+        // When:
+        val result = XmlUtils.stringResourceModelToElement(document, stringResourceModel, nsNameProvider)
+
+        // Then:
+        Truth.assertThat(result.textContent).isEqualTo("some content")
+        Truth.assertThat(result.attributes.length).isEqualTo(3)
+        Truth.assertThat(result.attributes.getNamedItem("name").textContent).isEqualTo("some_name")
+        Truth.assertThat(result.attributes.getNamedItem("extra").textContent).isEqualTo("some extra attr")
+        Truth.assertThat(result.attributes.getNamedItem("ns1:someNsKey").textContent).isEqualTo("Some namespaced value")
     }
 }
