f903553dd8aee5a988eab9e6c72ad0a852ac3fc9
Cesar Munoz <likethesalad@gmail.com>
2022-12-09 14:33:19 +0100

    Updated tests

--- stem-plugin/src/test/java/com/likethesalad/stem/data/storage/VariantBuildResolvedDirTest.kt
+++ stem-plugin/src/test/java/com/likethesalad/stem/data/storage/VariantBuildResolvedDirTest.kt
@@ -1,15 +1,11 @@
 package com.likethesalad.stem.data.storage
 
 import com.google.common.truth.Truth
-import com.likethesalad.stem.modules.common.helpers.dirs.SourceSetsHandler
 import com.likethesalad.stem.modules.common.helpers.dirs.VariantBuildResolvedDir
 import com.likethesalad.stem.providers.ProjectDirsProvider
 import com.likethesalad.tools.android.plugin.data.AndroidVariantData
-import io.mockk.Runs
 import io.mockk.every
-import io.mockk.just
 import io.mockk.mockk
-import io.mockk.verify
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TemporaryFolder
@@ -26,22 +22,16 @@ class VariantBuildResolvedDirTest {
         val buildDir = temporaryFolder.newFolder("build")
         val projectDirsProvider = mockk<ProjectDirsProvider>()
         val androidVariantData = mockk<AndroidVariantData>()
-        val sourceSetsHandler = mockk<SourceSetsHandler>()
         val newSrcDir = File(buildDir, "generated/resolved/$variantName")
         every { projectDirsProvider.getBuildDir() }.returns(buildDir)
         every { androidVariantData.getVariantName() }.returns(variantName)
-        every { sourceSetsHandler.addToSourceSets(newSrcDir, variantName) } just Runs
         val variantBuildResolvedDir = VariantBuildResolvedDir(
             projectDirsProvider,
-            sourceSetsHandler,
             androidVariantData
         )
 
         Truth.assertThat(variantBuildResolvedDir.resolvedDir).isEqualTo(
             newSrcDir
         )
-        verify {
-            sourceSetsHandler.addToSourceSets(newSrcDir, variantName)
-        }
     }
 }
---
+++ tests.sh
@@ -0,0 +1,4 @@
+./gradlew test
+./gradlew functionalTest
+./gradlew -p "stem-test" testDebugUnitTest -PagpTest_version=7.2.0
+./gradlew -p "stem-test" testDebugUnitTest -PagpTest_version=7.3.0
