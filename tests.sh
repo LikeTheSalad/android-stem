1580191f0d10d4934f99cf59f8cf7431b72e222c
Cesar Munoz <likethesalad@gmail.com>
2024-01-07 09:54:52 +0100

    Moving versions to the catalog

--- tests.sh
+++ tests.sh
@@ -1,5 +1,4 @@
 set -e
 ./gradlew test
 ./gradlew functionalTest
-./gradlew -p "stem-test" testDebugUnitTest -PagpTest_version=7.2.0
-./gradlew -p "stem-test" testDebugUnitTest -PagpTest_version=7.3.0
+./gradlew -p "stem-test" testDebugUnitTest
