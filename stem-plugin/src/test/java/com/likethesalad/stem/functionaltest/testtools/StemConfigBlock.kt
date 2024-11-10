d4f6f5864cc8c3975ae66c5693d8b646bec48383
Cesar Munoz <likethesalad@gmail.com>
2024-11-10 10:18:25 +0100

    Adding placeholder block to tests

--- stem-plugin/src/test/java/com/likethesalad/stem/functionaltest/testtools/StemConfigBlock.kt
+++ stem-plugin/src/test/java/com/likethesalad/stem/functionaltest/testtools/StemConfigBlock.kt
@@ -2,17 +2,25 @@ package com.likethesalad.stem.functionaltest.testtools
 
 import com.likethesalad.tools.functional.testing.blocks.GradleBlockItem
 
-class StemConfigBlock(private val includeLocalizedOnlyTemplates: Boolean? = null) : GradleBlockItem {
+class StemConfigBlock(
+    private val includeLocalizedOnlyTemplates: Boolean? = null,
+    private val placeholderBlock: PlaceholderBlock? = null
+) : GradleBlockItem {
 
     override fun getItemText(): String {
-        if (includeLocalizedOnlyTemplates == null) {
+        if (includeLocalizedOnlyTemplates == null && placeholderBlock == null) {
             return ""
         }
 
         val builder = StringBuilder()
 
         builder.appendLine("androidStem {")
-        builder.appendLine("includeLocalizedOnlyTemplates = $includeLocalizedOnlyTemplates")
+        if (includeLocalizedOnlyTemplates != null) {
+            builder.appendLine("includeLocalizedOnlyTemplates = $includeLocalizedOnlyTemplates")
+        }
+        if (placeholderBlock != null) {
+            builder.appendLine(placeholderBlock.getItemText())
+        }
         builder.appendLine("}")
 
         return builder.toString()
