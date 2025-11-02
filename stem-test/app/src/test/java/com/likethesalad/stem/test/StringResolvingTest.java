a687d5491407b7ad5634c9fa5e68b8c0e0a0d9e9
Cesar Munoz <likethesalad@gmail.com>
2025-11-02 12:43:56 +0100

    Updating tests

--- stem-test/app/src/test/java/com/likethesalad/stem/test/StringResolvingTest.java
+++ stem-test/app/src/test/java/com/likethesalad/stem/test/StringResolvingTest.java
@@ -9,7 +9,7 @@ import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.SpannedString;
 import android.text.style.StyleSpan;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
